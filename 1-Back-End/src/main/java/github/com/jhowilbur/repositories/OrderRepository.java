c07e5768f6467955a5b5b009dc549508b392f3f0
Jonathas Wilbur <=>
2021-01-06 05:23:35 -0300

    [POST] /orders

--- 1-Back-End/src/main/java/github/com/jhowilbur/repositories/OrderRepository.java
+++ 1-Back-End/src/main/java/github/com/jhowilbur/repositories/OrderRepository.java
@@ -9,8 +9,7 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long>
 {
-    //list by most oldest and only pending - JPQL
-
+    //list by most oldest and only pending
     String JPQLQuery = "SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products "
             + "Where obj.status = 0 ORDER BY obj.moment ASC";
 
--- 1-Back-End/src/main/java/github/com/jhowilbur/services/OrderService.java
+++ 1-Back-End/src/main/java/github/com/jhowilbur/services/OrderService.java
@@ -3,12 +3,15 @@ package github.com.jhowilbur.services;
 import github.com.jhowilbur.dto.OrderDTO;
 import github.com.jhowilbur.dto.ProductDTO;
 import github.com.jhowilbur.entities.Order;
+import github.com.jhowilbur.entities.OrderStatus;
 import github.com.jhowilbur.entities.Product;
 import github.com.jhowilbur.repositories.OrderRepository;
+import github.com.jhowilbur.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,12 +19,28 @@ import java.util.stream.Collectors;
 public class OrderService
 {
     @Autowired
-    private OrderRepository repository;
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
-        List<Order> list = repository.findOrdersWithProducts();
+        List<Order> list = orderRepository.findOrdersWithProducts();
         // for converting list from Product to ProductDTO
         return list.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+
+        for (ProductDTO p : dto.getProducts()) {
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+        order = orderRepository.save(order);
+        return new OrderDTO(order);
     }
 }
