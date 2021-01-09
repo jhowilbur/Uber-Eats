ec16d124c7af981da6c301a641bcb9dc197041c4
Jonathas Wilbur <=>
2021-01-09 01:30:59 -0300

    Insert Products with API

---
+++ 2-front-end/src/Orders/OrderLocation.tsx
@@ -0,0 +1,12 @@
+function OrderLocation() {
+
+    return (
+        <div className="order-location-container">
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default OrderLocation;
--- 2-front-end/src/Orders/index.tsx
+++ 2-front-end/src/Orders/index.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductCard from './ProductCard'
+import ProductsLists from './ProductsLists'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './types';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsLists products={products}/>
         </div>
     )
 }
